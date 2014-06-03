5bcb765076064643bb7bbfcdcf8b1fa43d990a72
maggarwal <maggarwal@amplify.com>
2014-06-02 22:05:13 -0400

    1.1

---
+++ Arrays_and_Strings/1-1.py
@@ -0,0 +1,18 @@
+# Q Implement an algorithm to datetime if a string has all unique chars. What if you cannot use additional data structures?
+def uniqueString(s):
+    # this one uses additional data structure
+    # complexity is n2
+    for i,a in enumerate(s):
+        if a in s[i+1:]:
+            return -1
+    return 1
+
+def uniqueStringWithHash(s):
+    uniqueHash = {}
+    for a in s:
+        if uniqueHash.get(a, False):
+            return False
+        uniqueHash[a] = True
+    return True
+print uniqueStringWithHash('asdf')
+# TODO learn the bit manipulation solution
