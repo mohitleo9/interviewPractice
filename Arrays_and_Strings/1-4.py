731c446b2a1352789df5989a4b41abb94154fe15
maggarwal <maggarwal@amplify.com>
2014-06-03 19:48:37 -0400

    1.4

---
+++ Arrays_and_Strings/1-4.py
@@ -0,0 +1,9 @@
+# Q replace all the occurances of space in a string with '%20'
+def replaceSpace(list, sub):
+    asdf = sub.join(list.split(' '))
+    print asdf
+
+
+
+
+replaceSpace(' asdf asdf  asdf  ', '111')
