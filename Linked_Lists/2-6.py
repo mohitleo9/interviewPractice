5ce3ad49e5d63c079ba9dc48e8b9ee0872271035
maggarwal <maggarwal@amplify.com>
2014-06-07 11:46:38 -0400

    skipping 2-6

---
+++ Linked_Lists/2-6.py
@@ -0,0 +1,14 @@
+# Q this is about detecting the loop in a linkedList and printing out where the loop is?
+from LinkedLists import LinkedList, Node
+
+
+def loop(l):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
